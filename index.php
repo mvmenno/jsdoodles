8981c6a075783bead1c7f32dc93a105125d6f6f8
Menno van Muilwijk <mennovanmuilwijk@hotmail.com>
2017-01-05 12:34:50 +0100

    init

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php 
+	echo 'Test';
+?>
+<html>
+	<head>
+		<title>Test</title>
+		<script src="lib/jquery-3.1.1.min.js"></script>
+		<script src="lib/three.min.js"></script>
+		<script src="js/main.js"></script>
+	</head>
+	<body>
+		<canvas id="gamecanvas"></canvas>
+	</body>
+</html>
